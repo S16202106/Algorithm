1f5ec0c942b83050659c74156febe63adc13eb28
S16202106 <758850574@qq.com>
2020-12-16 15:36:23 +0800

    更新规则

---
+++ src/com/company/layerForeachTree.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.tree.TreeNode;
+import java.util.List;
+
+public class layerForeachTree {
+
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            return null;
+        }
+
+        public void layerTree(List<List<Integer>> res, List<TreeNode> track) {
+
+            layerTree(res, track);
+        }
+
+    }
+}
