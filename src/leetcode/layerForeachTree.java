1ea39319237b0c528457bd7618193b48aa2c1d98
S16202106 <758850574@qq.com>
2020-12-16 15:32:39 +0800

    更新规则

--- src/leetcode/layerForeachTree.java
+++ src/leetcode/layerForeachTree.java
@@ -7,13 +7,11 @@ public class layerForeachTree {
 
     class Solution {
         public List<List<Integer>> levelOrder(TreeNode root) {
-
+            return null;
         }
 
         public void layerTree(List<List<Integer>> res, List<TreeNode> track) {
-            track.forEach(item -> (
 
-                    ));
             layerTree(res, track);
         }
 
