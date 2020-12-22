661f8a67a9a6ee1dcd5a78083df1c715d39201f4
S16202106 <758850574@qq.com>
2020-12-22 19:39:25 +0800

    二叉树的锯齿形遍历

---
+++ src/leetcode/backtracking/BinaryTreeForeach.java
@@ -0,0 +1,21 @@
+package leetcode.backtracking;
+
+import leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeForeach {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> each(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        return res;
+    }
+
+    public static void back() {
+
+    }
+}
--- src/leetcode/dp/subString.java
+++ src/leetcode/dp/subString.java
@@ -1,4 +1,19 @@
 package leetcode.dp;
 
+import javax.management.DescriptorAccess;
+import java.text.DecimalFormat;
+
 public class subString {
+    public static void main(String[] args) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        Double t = Double.MAX_VALUE;
+//        if(t==Double.MAX_VALUE)
+        if (t.isInfinite())
+
+            System.out.println(t);
+        System.out.println(t);
+        if (t.isNaN())
+            System.out.println(t);
+        System.out.println(decimalFormat.format(t));
+    }
 }
