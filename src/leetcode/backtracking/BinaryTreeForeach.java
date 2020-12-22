4598483aaa3d36efc36e052f76f09c191d9278fb
S16202106 <758850574@qq.com>
2020-12-22 20:11:49 +0800

    二叉树的锯齿形遍历

--- src/leetcode/backtracking/BinaryTreeForeach.java
+++ src/leetcode/backtracking/BinaryTreeForeach.java
@@ -15,7 +15,19 @@ public class BinaryTreeForeach {
         return res;
     }
 
-    public static void back() {
+    public static void back(List<TreeNode> next, boolean left) {
+        if (next.size() < 1) {
 
+        } else {
+            List<TreeNode> track = new ArrayList<>();
+            for (int i = next.size() - 1; i > -1; i--) {
+                TreeNode first = next.get(i).left;
+                TreeNode second = next.get(i).right;
+                if (!left) {
+                    track.add(next.get(i).left);
+                    track.add(next.get(i).right);
+                }
+            }
+        }
     }
 }
